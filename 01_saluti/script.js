bb43db61843b24312e8b120d3ae4176a1a640be2
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-09 13:34:24 +0100

    first js snack doone

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,20 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
 
+/**
+ * This function greets the user
+ * @param {string} name user's name
+ * @returns {string}
+ */
+function greet(name) {
+  console.log("Ciao", name);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+greet(userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
