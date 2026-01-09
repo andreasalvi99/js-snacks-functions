e75ba156c840232bc59de704b91aba20556d6098
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-01-09 13:49:52 +0100

    definita la funzione della seconda task

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,23 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+/**
+ *
+ * @param {string} letter the first letter ov every array's item
+ * @returns {string} only the first letter of every array's item
+ */
+function pickFirstLetter(letter) {
+  for (let i = 0; i < names.length; i++) {
+    const currentName = names[i];
+    const firstLetter = currentName.charAt(0);
+    console.log("firstLetter:", firstLetter);
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+pickFirstLetter();
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
